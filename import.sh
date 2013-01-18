b2cc92752e32fd9dcb606114a8e7d3b9fc5b7d55
Jari Bakken <jari.bakken@gmail.com>
2013-01-18 09:47:43 +0100

    update

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@ set -e
 
 rake clean
 TARGETS="build/java/server/test/org/openqa/selenium/server-with-tests-standalone.jar //rb:gem:build"
-export SELENIUM_ROOT=$HOME/src/selenium
+export SELENIUM_ROOT=$HOME/src/git/selenium
 cd $SELENIUM_ROOT
 ./go $TARGETS
 cd -
