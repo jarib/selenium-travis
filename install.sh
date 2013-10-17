f550c4666f4cda9fb6edc4501e30fcef0d7cb24d
Jari Bakken <jari.bakken@gmail.com>
2013-10-17 21:57:07 +0200

    Upgrade to chromedriver 2

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 set -x
 
-CHROME_REVISION=142538
+CHROME_REVISION="229207"
+CHROMEDRIVER_VERSION="2.4"
 sh -e /etc/init.d/xvfb start
 
 if [[ "$WD_SPEC_DRIVER" = "chrome" ]]; then
   sudo apt-get install -y unzip libxss1
   curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.zip"
   unzip chrome-linux.zip
-  curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
+  curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+  mv chromedriver_linux64/chromedriver chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
   sudo chmod 1777 /dev/shm
 fi
