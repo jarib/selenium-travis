bc35b69dc6882558b01d47fa0a587ffdaa90478a
Jari Bakken <jari.bakken@gmail.com>
2013-10-17 22:49:04 +0200

    Yet another chromedriver install fix.

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ if [[ "$WD_SPEC_DRIVER" = "chrome" ]]; then
   curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.zip"
   unzip chrome-linux.zip
   curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+  unzip chromedriver_linux64.zip
   mv chromedriver chrome-linux/chromedriver
   chmod +x chrome-linux/chromedriver
   sudo chmod 1777 /dev/shm
